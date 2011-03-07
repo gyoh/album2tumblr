347dd30fdd421dead78fcec8c28c6506b34dce27
Gyo Hamamoto <gyo@hamamoto.org>
2011-03-08 02:43:01 +0900

    Removed unnecessary import statements.

--- src/main/scala/Album2Tumblr.scala
+++ src/main/scala/Album2Tumblr.scala
@@ -9,12 +9,7 @@ import java.util.{ArrayList, Calendar}
 
 import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.http.client.methods.HttpPost
-import org.apache.http.params.BasicHttpParams
 import org.apache.http.entity.BufferedHttpEntity
-import org.apache.http.entity.StringEntity
-import org.apache.http.client.entity.UrlEncodedFormEntity
-import org.apache.http.NameValuePair
-import org.apache.http.message.BasicNameValuePair
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
